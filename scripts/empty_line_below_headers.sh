11595dc3a7bdd869462c30250435c3ff53dd52fa
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2026-06-01 23:26:40 +0200

    Create empty_line_below_headers.sh

---
+++ scripts/empty_line_below_headers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+find . -type f -print0 |
+while IFS= read -r -d '' file; do
+  sed -i -E '
+    /^```/ {
+      b
+    }
+
+    /^[[:space:]]*#/ {
+      N
+      s/^([^\n]*#.*)\n[[:space:]]*$/\1\n/
+      t
+      s/^([^\n]*#.*)\n([[:space:]]*\n)+/\1\n\n/
+      t
+      s/^([^\n]*#.*)\n([^\n].*)$/\1\n\n\2/
+    }
+  ' "$file"
+done
